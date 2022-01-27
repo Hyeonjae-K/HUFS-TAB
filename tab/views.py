3d8721044f39ae6b7fcfe85463562411e675f2c4
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-01-27 14:48:33 +0900

    FEAT - pagination 구현

--- tab/views.py
+++ tab/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.utils import timezone
 
 from os import listdir
@@ -19,8 +20,10 @@ def index(request):
 
 
 def board(request):
+    page = request.GET.get('page', '1')
     contents = Contents.objects.order_by('-create_date')
-    context = {'contents': contents}
+    paginator = Paginator(contents, 10)
+    context = {'contents': paginator.get_page(page)}
     return render(request, 'tab/board.html', context)
 
 

96293c603ef4cceacd5fee8591dce6a371dc65e0
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-02-01 21:52:37 +0900

    FEAT - 로그인 세션 타임아웃 기능 추가

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+
+# Login session timeout
+SESSION_COOKIE_AGE = 300
+
+SESSION_SAVE_EVERY_REQUEST = True

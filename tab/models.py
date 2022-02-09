7b0b2a908d5bb6cf3147a5ace727dd14084bf533
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-02-09 14:59:00 +0900

    FEAT - 모델명 수정 및 지원서 파일명 변경 기능 추가

--- tab/models.py
+++ tab/models.py
@@ -3,22 +3,40 @@ from django.utils import timezone
 from django.core.exceptions import ValidationError
 
 
-class Contents(models.Model):
+class Post(models.Model):
     subject = models.CharField(max_length=16)
     content = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
 
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
 
-class Applications(models.Model):
+    def __str__(self):
+        return self.subject
+
+
+class Application(models.Model):
     def _validate_file(value):
         value = str(value)
         if value.endswith('.pdf') or value.endswith('.doc') or value.endswith('.docx') or value.endswith('.txt'):
             return value
-        raise ValidationError("Not suppor file format")
+        raise ValidationError("지원하지 않는 파일 형식입니다.")
+
+    def _set_file_path(instance, filename):
+        file_format = filename.split('.')[-1]
+        return f'applications/%Y-%m/{instance.studentnumber}_{instance.name}.{file_format}'
 
     name = models.CharField(max_length=8)
     studentnumber = models.CharField(max_length=16)
     phonenumber = models.CharField(max_length=11)
-    path = models.FileField(upload_to='applications/%Y-%m',
-                            validators=[_validate_file])
+    file_path = models.FileField(upload_to=_set_file_path,
+                                 validators=[_validate_file])
     create_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'Application'
+        verbose_name_plural = 'Applications'
+
+    def __str__(self):
+        return f'{self.studentnumber}_{self.name}'
